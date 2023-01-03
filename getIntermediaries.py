bc8151079ef5f7dfa0e91ad010c51b9f65b89dc4
Kirby1997 <jj@wilkinsons.me.uk>
2023-01-03 20:24:57 +0000

    Add saving of results and progress of runs

--- getIntermediaries.py
+++ getIntermediaries.py
@@ -3,44 +3,45 @@ import json
 import sqlite3
 import os
 import bananopy.banano as ban
+import timeit
+import math
+import datetime
 
 
-def get_history(address, noTrans = 20):
-    pagesize = 5000
+def get_history(address, noTrans=20, pagesize=3000):
 
-    if noTrans <= pagesize:
-        history = ban.account_history(address, noTrans)
-        return history["history"]
+    if pagesize > 3000:
+        pagesize = 3000
 
-    else:
-
-        remainder = (noTrans % pagesize)
-        pages = (noTrans - remainder) / pagesize
-        count = 0
-        offset = 0
-        full_history = []
-        accountInfo = ban.account_info(address)
+    accountInfo = ban.account_info(address)
+    head = getLastRun(address)
+    if head is None:
         head = accountInfo["open_block"]
-        frontier = accountInfo["frontier"]
-        blockHeight = accountInfo["block_count"]
+    frontier = accountInfo["frontier"]
+    if head == frontier:
+        pass
+    blockHeight = accountInfo["block_count"]
 
-        while count < pages:
-            print("Downloading history: Page " + str(count+1) + "/" + str(pages))
-            history = ban.account_history(address, pagesize, head=head, reverse=True)
+    pages = math.ceil(noTrans / pagesize)
+    count = 0
+    full_history = []
 
-            count = count + 1
-            full_history = full_history + history["history"]
-            print(len(history["history"]))
-
+    while count < pages:
+        transactions_needed = noTrans - len(full_history)
+        transactions_per_page = math.ceil(transactions_needed / (pages - count))
+        print("Downloading history for {}: Page {}/{}".format(address, count + 1, pages))
+        history = ban.account_history(address, transactions_per_page, head=head, reverse=True)
+        count += 1
+        full_history += history["history"]
+        try:
             head = history["next"]
+        except Exception as e:
+            print(e)
+            head = frontier
 
-        if remainder != 0:
-            history = ban.account_history(address, remainder, head=head, reverse=True)
-            head = history["history"][len(history["history"]) - 1]["hash"]
-            full_history = full_history + history["history"]
+        writeLastRun(address, head)
 
-        print(len(full_history))
-        return full_history
+    return full_history
 
 
 def dict_factory(cursor, row):
@@ -50,25 +51,93 @@ def dict_factory(cursor, row):
     return d
 
 
-def readDatabase():
-    conn = sqlite3.connect(os.getcwd() + "/addresses.sqlite")
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_addresses = cur.execute('SELECT * FROM addresses WHERE type=\'Exchange\' OR type=\'Gambling\' OR alias=\'Tip[.]cc\';').fetchall()
+
+def readServices():
+    with sqlite3.connect(os.getcwd() + "/addresses.sqlite") as conn:
+        types = ["Exchange", "Gambling"]
+        aliases = ["Tip[.]cc"]
+        excludeadd = ["ban_1wu6rxojhgjwy1mzci1jmkm7az7nntf31h6ptuoqmetwp64j3sxo7het6e3m",
+                   "ban_1eub1ezmefs1im56oomprzddwhp5s9c6brhx755ftjfjz45uytputarmcwto",
+                      "ban_15sxnajaaztcbeewrs7yyih4teiz4jsxur1j393yi8xhytuza5jty6emdogo",
+                      "ban_3sprts3a8hzysquk4mcy4ct4f6izp1mxtbnybiyrriprtjrn9da9cbd859qf",
+                      "ban_1pd6agpcfiweyhcyyej7cwpb9gymhsnftr6nyihrqtib51zqps8har57wyur",
+                      "ban_1swapdwa9wwaxh38htsrupc6zfrcojtqz4kd6jssrat6cec1ggfjnnm6hypz",
+                      "ban_1t8hyu6taczfq3gmdqkwcxc6z4pt3sfwg5m7op488hxjmne55r16tebaoztb",
+                      "ban_3r7xjnq4ywn1sf387xzpgbytwuaa6hgfurd11mx7qagkx1hiuq73ka63hbxz", #banlotto
+                      "ban_1kwin96znfqopi7be3shxcxn8qeruirob885oaya4ix5pkrnpsou4u5qbeaa" #banslots
+                      ]
+
+        params = []
+        [params.extend(l) for l in (types,aliases,excludeadd)]
+
+
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+
+        query = 'SELECT address FROM addresses'
+        query += ' WHERE (type IN ({}) OR alias IN ({}))'.format(', '.join('?' * len(types)),
+                                                                 ', '.join('?' * len(aliases)))
+        query += ' AND address NOT IN ({})'.format(', '.join('?' * len(excludeadd)))
+
+
+
+        all_addresses = cur.execute(query, params).fetchall()
+        #all_addresses = cur.execute('SELECT * FROM addresses WHERE type=\'Exchange\' OR type=\'Gambling\' OR alias=\'Tip[.]cc\';').fetchall()
 
     return json.dumps(all_addresses)
 
+def writeIntermediaries(newintermediaries):
+    with sqlite3.connect(os.getcwd() + "/addresses.sqlite") as conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS intermediaries (address TEXT, service TEXT)")
+
+        for service, addresses in newintermediaries.items():
+            for address in addresses:
+                try:
+                    cur.execute("INSERT INTO intermediaries (address, service) VALUES (?, ?)", (address, service))
+                except sqlite3.IntegrityError:
+                    # ignore errors when inserting non-unique values
+                    pass
+
+        conn.commit()
+
+
+def getLastRun(address):
+    with sqlite3.connect(os.getcwd() + "/addresses.sqlite") as conn:
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+        query = 'SELECT head FROM last_run WHERE address=?'
+        head = cur.execute(query, (address,)).fetchone()
+        if head is not None:
+            return head['head']
+        return None
+
+def writeLastRun(address, head):
+    with sqlite3.connect(os.getcwd() + "/addresses.sqlite") as conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS last_run (address TEXT PRIMARY KEY, date_time TEXT, head TEXT)")
+
+        date_time = str(datetime.datetime.now())
+        cur.execute("UPDATE last_run SET date_time=?, head=? WHERE address=?", (date_time, head, address))
+
+        conn.commit()
+
 
 def main():
-    mainAddresses = readDatabase()
+    mainAddresses = readServices()
     mainAddresses = json.loads(mainAddresses)
-    #for address in mainAddresses:
-    print(mainAddresses[0]["address"])
-    #history = get_history(mainAddresses[0]["address"], noTrans=20)
-    history = get_history("ban_31dhbgirwzd3ce7naor6o94woefws9hpxu4q8uxm1bz98w89zqpfks5rk3ad", noTrans=10000)
+    newintermediaries = {}
+    for record in mainAddresses:
+        print(record["address"])
+        mainaddress = record["address"]
+        intermediaries = []
+        history = get_history(record["address"], noTrans=20)
+        for transaction in history:
+            if transaction["account"] not in intermediaries:
+                intermediaries.append(transaction["account"])
+        newintermediaries[record["address"]] = intermediaries
+    writeIntermediaries(newintermediaries)
 
-    print(history)
-    print(len(history))
 
 
 main()
